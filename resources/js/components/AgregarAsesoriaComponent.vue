fba19a50c944538f8b76aaf023d96a4a2056fe04
Gabriel Flores <gabriel.floresra@gmail.com>
2019-11-01 15:21:57 -0600

    agregada interfaz con el esatdo de asesorias agendadas y agendar nueva asesoria

---
+++ resources/js/components/AgregarAsesoriaComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Agendar Asesoria</div>
+
+                    <div class="card-body">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Sistema de Citas Para Asesorias
                 </div>
 
                 <div class="links">
