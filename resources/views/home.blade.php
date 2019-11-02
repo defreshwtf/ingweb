0ffffcd11beaedf2a5da035036957704527e9828
Gabriel Flores <gabriel.floresra@gmail.com>
2019-11-01 19:45:02 -0600

    todo alv compa :v

--- resources/views/home.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <agregar-asesoria-component></agregar-asesoria-component>
-    <estado-asesorias-component></estado-asesorias-component>
-@endsection
