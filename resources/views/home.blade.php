8d0cc0f7e114a08c958bf34345353a2dba2ea965
Gabriel Flores <gabriel.floresra@gmail.com>
2019-11-01 19:46:30 -0600

    todo de la verga :v

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <agendar-asesoria-component></asesorias-agendadas-component>
+    <asesorias-agendadas-component></asesorias-agendadas-component>
+@endsection
