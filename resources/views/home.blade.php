bcca9719574f449c0f095fc04173cb33f5d7530f
Gabriel Flores <gabriel.floresra@gmail.com>
2019-11-02 23:54:52 -0600

    inicia migracion de bootstrap a vuetufy

--- resources/views/home.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <agendar-asesoria-component></asesorias-agendadas-component>
-    <asesorias-agendadas-component></asesorias-agendadas-component>
-@endsection
